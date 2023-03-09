b1738500b3f26ae99f642b3002232ee5a1b94927
binita44 <binu.poudel31@gmail.com>
2023-03-08 23:25:46 -0500

    practice code commit

---
+++ src/basicJavaConcept/StringReverse.java
@@ -0,0 +1,14 @@
+package basicJavaConcept;
+
+public class StringReverse {
+	public static void reverse(String name) {
+		 
+		
+		
+		
+	}
+	public static void main (String [] args) {
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ujjwalbastola
+ *
+ */
+module Practice {
+}
